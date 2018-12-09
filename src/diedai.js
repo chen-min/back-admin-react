1f0faa92b4fd925fe28612fb4da940f17d304cc2
chenm <chenmin@hotmail.de>
2018-12-09 12:56:55 +0800

    recursion

--- src/diedai.js
+++ src/diedai.js
@@ -46,26 +46,47 @@
 // console.log([...flatten([1,2,[3,4,[[5]]]])])
 // //[ 1, 2, 3, 4, 5 ]
 
-function request(url){
-  return cb  => {
-    setTimeout( () => {
-      cb(Math.random())
-    }, 1000)
-  }
-}
-create_runner(function*(){
-  const val1 = yield request('some url')
-  const val2 = yield request('some url')
-  console.log(val1,val2)
-})()
+// function request(url){
+//   return cb  => {
+//     setTimeout( () => {
+//       cb(Math.random())
+//     }, 1000)
+//   }
+// }
+// create_runner(function*(){
+//   const val1 = yield request('some url')
+//   const val2 = yield request('some url')
+//   console.log(val1,val2)
+// })()
+// e
+// function create_runner(genFunc) {
+//   const it = genFunc()
+//   function run(data) {
+//     const itVal = it.next(data)
+//     if(!itVal.done){
+//       itVal.value(run)
+//     }
+//   }
+//   return run
+// }
 
-function create_runner(genFunc) {
-  const it = genFunc()
-  function run(data) {
-    const itVal = it.next(data)
-    if(!itVal.done){
-      itVal.value(run)
-    }
-  }
-  return run
+function cartesian_product(Matrix) {
+  if(Matrix.length === 0 ) return []
+  if(Matrix.length === 1 ) return Matrix[0]
+  return Matrix.reduce((A, B) => {
+    const product = []
+    for(let i = 0; i < A.length; i++)
+      for(let j = 0; j < B.length; j++) {
+        product.push(Array.isArray(A[i]) ? [...A[i], B[j]] : [A[i], B[j]])
+      }
+    return product
+  })
 }
+const aaa = cartesian_product([[1,[4,5],2,3],['a','b','c']])
+console.log(aaa,'aaa')
+
+[“王成成”,  ”王峰”,  ”蒋雪”,  ”李明”].sort((a,b) => a.localeCompare(b,’zh’))
+
+
+
+
